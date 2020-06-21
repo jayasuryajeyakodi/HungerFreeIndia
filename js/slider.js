2c28e73b56542695a03b75136b2e1ae24273a345
thirulogachandar <thirulogachandarkt.gmail.com>
2020-06-21 15:28:24 +0530

    images added and required changes are made

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,20 @@
-
-var slideIndex = 0;
-showSlides();
+const slides = document.getElementsByClassName("mySlides");
+let slideIndex = 0;
+for (let i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+}
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
   slideIndex++;
+
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
 
   slides[slideIndex - 1].style.display = "block";
-
-  setTimeout(showSlides, 2000); 
+  setTimeout(() => {
+    slides[slideIndex - 1].style.display = "none";
+    showSlides();
+  }, 5000);
 }
-
+showSlides();
