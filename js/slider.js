9053f2fc481a798937e9847704e42446f1249e3b
thirulogachandar <thirulogachandarkt.gmail.com>
2020-06-22 16:02:11 +0530

    loop removed

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,5 @@
 const slides = document.getElementsByClassName("mySlides");
 let slideIndex = 0;
-for (let i = 0; i < slides.length; i++) {
-  slides[i].style.display = "none";
-}
 
 function showSlides() {
   slideIndex++;
